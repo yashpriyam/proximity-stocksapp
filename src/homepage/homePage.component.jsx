e6729e9d6dfca04fd1d53cfdbac70b5a15a1d6a4
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-11-01 00:57:13 +0530

    trying a workaround for ws issue

--- src/homepage/homePage.component.jsx
+++ src/homepage/homePage.component.jsx
@@ -11,10 +11,11 @@ const HomePage = () => {
 
   console.log(`appState: ${appState}`);
   let updateState = [...appState];
+  const port = process.env.PORT || 3001
 
 
   useEffect(() => {
-    webSocket.current = new WebSocket("wss://stocks.mnet.website");
+    webSocket.current = new WebSocket(`wss://stocks.mnet.website:${port}`);
     webSocket.current.onopen = () => {
       console.log("connection opened");
     };
